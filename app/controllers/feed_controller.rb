68917e48f50639add42a8e5d71c9ba5bfca65076
佐藤一輝 <i.tennis0210@gmail.com>
2015-09-24 15:17:02 -0700

    develop parser

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,29 +1,31 @@
+require 'open-uri'
+require 'nokogiri'
+
 class FeedController < ApplicationController
   def index
+	url = 'http://newsbiz.yahoo.co.jp/search?p=%E5%8C%97%E7%B1%B3%E9%80%B2%E5%87%BA+OR+%E5%8C%97%E7%B1%B3%E5%B1%95%E9%96%8B+OR+%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E9%80%B2%E5%87%BA+OR+%E7%B1%B3%E5%9B%BD%E5%B1%95%E9%96%8B+OR+%E7%B1%B3%E5%9B%BD%E9%80%B2%E5%87%BA+OR+US%E9%80%B2%E5%87%BA+OR+%E6%B5%B7%E5%A4%96%E5%B1%95%E9%96%8B+OR+%E6%B5%B7%E5%A4%96%E9%80%B2%E5%87%BA&vaop=a&to=0&ca=ecny&ca=etp&ca=mkt&ca=g_int&ca=cn&ca=asa&ca=eus&ind=agr&ind=cst&ind=fds&ind=egy&ind=chm&ind=med&ind=mch&ind=eqp&ind=aut&ind=ot_mfr&ind=trs&ind=inf&ind=mda&ind=lgt&ind=fince&ind=svc&at=n'
 
-  	@feeds = Array.new(3)
+  	charset = nil
+  	html = open(url) do |f|
+    	charset = f.charset
+		f.read
+	end
 
-  	feed1 = Feed.new
-  	feed1.title = "title1" 
-  	feed1.url = "http://d.hatena.ne.jp/favril/20100604/1275668631"
-  	feed1.desc = "dddddddddddddescescescescescescescescescescescescesc"
-  	feed1.created_at = DateTime.new(2015, 9, 24, 6, 00, 45, 0.375)
+	doc = Nokogiri::HTML.parse(html, nil, charset)
 
-  	feed2 = Feed.new
-  	feed2.title = "title2" 
-  	feed2.url = "https://github.com/btrax/watson_news/pull/3"
-  	feed2.desc = "dddddddddddddescescescescescescescescescescescescesc"
-  	feed2.created_at = DateTime.new(2015, 9, 24, 6, 00, 45, 0.375)
+	@feeds = []
+	doc.xpath('//div[@class="newsListBody"]').each do |newsList|
+		newsList.xpath('//dl[@class="srchBox"]').each_with_index do |box,count|
+			feed = Feed.new
+			feed.title = box.xpath('//dt//p//a')[count].text
+			feed.url = box.xpath('//dt//p//a')[count].attr('href')
+			feed.desc = box.xpath('//dd[@class="listDetail"]/p')[count].text
+			time = box.xpath('//dd[@class="newsSupple"]/span')[count].text
+			feed.created_at = DateTime.parse(time)
 
-  	feed3 = Feed.new
-  	feed3.title = "title3" 
-  	feed3.url = "https://github.com/btrax/watson_news/pull/3"
-  	feed3.desc = "dddddddddddddescescescescescescescescescescescescesc"
-  	feed3.created_at = DateTime.new(2015, 9, 25, 6, 00, 45, 0.375)
-
-  	@feeds[0] = feed1
-  	@feeds[1] = feed2
-  	@feeds[2] = feed3
+			@feeds.push(feed)
+		end
+	end
 
 	respond_to do |format|
 		format.html
