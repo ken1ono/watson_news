e3cc5d4bc2f42955281d0b41edca0b7fa1672f31
shiga yuichi <yuichi.shiga@gmail.com>
2015-09-28 11:42:08 -0700

    add keyword

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 class FeedController < ApplicationController
   def index
-	url = 'http://newsbiz.yahoo.co.jp/search?p=%E5%8C%97%E7%B1%B3%E9%80%B2%E5%87%BA+OR+%E5%8C%97%E7%B1%B3%E5%B1%95%E9%96%8B+OR+%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E9%80%B2%E5%87%BA+OR+%E7%B1%B3%E5%9B%BD%E5%B1%95%E9%96%8B+OR+%E7%B1%B3%E5%9B%BD%E9%80%B2%E5%87%BA+OR+US%E9%80%B2%E5%87%BA+OR+%E6%B5%B7%E5%A4%96%E5%B1%95%E9%96%8B+OR+%E6%B5%B7%E5%A4%96%E9%80%B2%E5%87%BA&vaop=a&to=0&ca=ecny&ca=etp&ca=mkt&ca=g_int&ca=cn&ca=asa&ca=eus&ind=agr&ind=cst&ind=fds&ind=egy&ind=chm&ind=med&ind=mch&ind=eqp&ind=aut&ind=ot_mfr&ind=trs&ind=inf&ind=mda&ind=lgt&ind=fince&ind=svc&at=n'
+	url = 'http://newsbiz.yahoo.co.jp/search?p=%E5%8C%97%E7%B1%B3%E9%80%B2%E5%87%BA+OR+%E5%8C%97%E7%B1%B3%E5%B1%95%E9%96%8B+OR+%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E9%80%B2%E5%87%BA+OR+%E7%B1%B3%E5%9B%BD%E5%B1%95%E9%96%8B+OR+%E7%B1%B3%E5%9B%BD%E9%80%B2%E5%87%BA+OR+US%E9%80%B2%E5%87%BA+OR+%E6%B5%B7%E5%A4%96%E5%B1%95%E9%96%8B+OR+%E6%B5%B7%E5%A4%96%E9%80%B2%E5%87%BA+OR+%E3%82%B7%E3%83%AA%E3%82%B3%E3%83%B3%E3%83%90%E3%83%AC%E3%83%BC+OR+%E3%82%A4%E3%83%8E%E3%83%99%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3+OR+%E3%83%99%E3%82%A4%E3%82%A8%E3%83%AA%E3%82%A2&to=0&ca=ecny&ca=etp&ca=mkt&ca=g_int&ca=cn&ca=asa&ca=eus&ind=agr&ind=cst&ind=fds&ind=egy&ind=chm&ind=med&ind=mch&ind=eqp&ind=aut&ind=ot_mfr&ind=trs&ind=inf&ind=mda&ind=lgt&ind=fince&ind=svc&at=n&sort=new'
 
   	charset = nil
   	html = open(url) do |f|
