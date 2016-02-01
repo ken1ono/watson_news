2b70ce9d5b29400281a6288b5896edeaea8443e4
IzumiSy <beetle-noise@gmx.com>
2016-02-01 14:38:46 -0800

    Gain more readability and modifiability

--- app/controllers/concerns/feed_generator.rb
+++ app/controllers/concerns/feed_generator.rb
@@ -1,10 +1,37 @@
+require 'uri'
 require 'open-uri'
 require 'nokogiri'
 
 module FeedGenerator
   def self.build_yahoo_biz_rss
     charset = nil
-    yahoo_biz_news_url = 'http://newsbiz.yahoo.co.jp/search?p=%E5%8C%97%E7%B1%B3%E9%80%B2%E5%87%BA+OR+%E5%8C%97%E7%B1%B3%E5%B1%95%E9%96%8B+OR+%E3%82%A2%E3%83%A1%E3%83%AA%E3%82%AB%E9%80%B2%E5%87%BA+OR+%E7%B1%B3%E5%9B%BD%E5%B1%95%E9%96%8B+OR+%E7%B1%B3%E5%9B%BD%E9%80%B2%E5%87%BA+OR+US%E9%80%B2%E5%87%BA+OR+%E6%B5%B7%E5%A4%96%E5%B1%95%E9%96%8B+OR+%E6%B5%B7%E5%A4%96%E9%80%B2%E5%87%BA+OR+%E3%82%B7%E3%83%AA%E3%82%B3%E3%83%B3%E3%83%90%E3%83%AC%E3%83%BC+OR+%E3%82%A4%E3%83%8E%E3%83%99%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3+OR+%E3%83%99%E3%82%A4%E3%82%A8%E3%83%AA%E3%82%A2+OR+btrax+OR+%E3%83%93%E3%83%BC%E3%83%88%E3%83%A9%E3%83%83%E3%82%AF%E3%82%B9+OR+%E3%82%AF%E3%83%A9%E3%82%A6%E3%83%89%E3%83%95%E3%82%A1%E3%83%B3%E3%83%87%E3%82%A3%E3%83%B3%E3%82%B0&to=0&ca=ecny&ca=etp&ca=mkt&ca=g_int&ca=cn&ca=asa&ca=eus&ind=agr&ind=cst&ind=fds&ind=egy&ind=chm&ind=med&ind=mch&ind=eqp&ind=aut&ind=ot_mfr&ind=trs&ind=inf&ind=mda&ind=lgt&ind=fince&ind=svc&at=n&sort=new'
+    query_set = [
+      "北米進出",
+      "北米展開",
+      "アメリカ進出",
+      "米国展開",
+      "米国進出",
+      "US進出",
+      "海外展開",
+      "海外進出",
+      "シリコンバレー",
+      "イノベーション",
+      "ベイエリア",
+      "btrax",
+      "ビートラックス",
+      "クラウドファンディング"
+    ]
+
+    query_string = ""
+    query_set.each_with_index do |query, index|
+      query_string << query;
+      query_string << "+OR+" if index < (query_set.length - 1)
+    end
+
+    yahoo_biz_news_url =
+      "http://newsbiz.yahoo.co.jp/search?p=#{URI.escape(query_string)}"+
+      "&to=0&ca=ecny&ca=etp&ca=mkt&ca=g_int&ca=cn&ca=asa"+
+      "&ca=eus&ind=agr&ind=cst&ind=fds&ind=egy&ind=chm&ind=med"
 
     html = open(yahoo_biz_news_url) do |f|
       charset = f.charset
