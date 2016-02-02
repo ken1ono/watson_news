0affb4ff3ffaa9233cfbd6d8be421a19644cb46d
IzumiSy <beetle-noise@gmx.com>
2016-02-02 13:17:51 -0800

    Add basement skelton for build_english_news_rss(...)

--- app/controllers/concerns/feed_generator.rb
+++ app/controllers/concerns/feed_generator.rb
@@ -33,6 +33,7 @@ module FeedGenerator
       "&to=0&ca=ecny&ca=etp&ca=mkt&ca=g_int&ca=cn&ca=asa"+
       "&ca=eus&ind=agr&ind=cst&ind=fds&ind=egy&ind=chm&ind=med"
 
+    charset = nil
     html = open(yahoo_biz_news_url) do |f|
       charset = f.charset
       f.read
@@ -41,11 +42,14 @@ module FeedGenerator
     parse_yahoo_html(html, charset)
   end
 
+  def self.build_english_news_rss()
+    []
+  end
+
   private
 
   def self.parse_yahoo_html(html, charset)
     feeds = []
-    charset = nil
     doc = Nokogiri::HTML.parse(html, nil, charset)
 
     doc.xpath('//div[@class="newsListBody"]').each do |newsList|
